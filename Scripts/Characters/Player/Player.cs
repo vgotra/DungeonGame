9a1e6eea0eeaa3581e8d375744e8a5598e9a5ab5
Volodymyr Gotra <vgotra@gmail.com>
2024-12-28 14:58:23 +0200

    Added basic movement of player, etc

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -1,6 +1,17 @@
-using Godot;
-using System;
-
 public partial class Player : CharacterBody3D
 {
+    private const float Speed = 4;
+    private  Vector2 _direction = Vector2.Zero;
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = new Vector3(_direction.X, 0, _direction.Y) * Speed;
+        MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        //NOTE: Add options to Inverse movement of the player
+        _direction = Input.GetVector("MoveLeft", "MoveRight", "MoveBackward", "MoveForward");
+    }
 }
