6adecb99b2e603826468a297203a6b00c57115eb
Volodymyr Gotra <vgotra@gmail.com>
2025-01-01 18:54:31 +0200

    Updated plugin for importing animations, etc

--- Addons/importanimationsplugin/ImportAnimationsPlugin.cs
+++ Addons/importanimationsplugin/ImportAnimationsPlugin.cs
@@ -3,57 +3,58 @@
 [Tool]
 public partial class ImportAnimationsPlugin : EditorPlugin
 {
-    private readonly Button _generateButton = new() { Text = "Create Animation for Sprite3D" };
-    private readonly FileDialog _fileDialog = new() { FileMode = FileDialog.FileModeEnum.OpenDir };
+	private Button _generateButton;
 
-    public override void _EnterTree()
-    {
-        _generateButton.Pressed += OnGenerateButtonPressed;
-        AddControlToContainer(CustomControlContainer.Toolbar, _generateButton);
-    }
+	public override void _EnablePlugin()
+	{
+		base._EnablePlugin();
+		GD.Print("Loaded plugin: ImportAnimationsPlugin");
+		_generateButton = new Button { Text = "Import Animations" };
+		_generateButton.Pressed += OnGenerateButtonPressed;
+		AddControlToContainer(CustomControlContainer.Toolbar, _generateButton);
+	}
 
-    public override void _ExitTree()
-    {
-        _fileDialog.DirSelected -= OnFolderSelected;
-        _generateButton.Pressed -= OnGenerateButtonPressed;
-        RemoveControlFromContainer(CustomControlContainer.Toolbar, _generateButton);
-        _fileDialog.QueueFree();
-        _generateButton.QueueFree();
-    }
+	public override void _DisablePlugin()
+	{
+		base._DisablePlugin();
+		GD.Print("Unloaded plugin: ImportAnimationsPlugin");
+		RemoveControlFromContainer(CustomControlContainer.Toolbar, _generateButton);
+		_generateButton.Pressed -= OnGenerateButtonPressed;
+		_generateButton.Free();
+		_generateButton = null;
+	}
 
-    private void OnGenerateButtonPressed()
-    {
-        _fileDialog.DirSelected += OnFolderSelected;
-        _fileDialog.PopupCentered();
-    }
+	private void OnGenerateButtonPressed()
+	{
+		var folderPath = ProjectSettings.GlobalizePath("res://Assets/Sprites/Characters/Player");
+		GD.Print("OnFolderSelected: " + folderPath);
 
-    private void OnFolderSelected(string folderPath)
-    {
-        GD.Print("OnFolderSelected: " + folderPath);
-        return;
+		var files = Directory.GetFiles(folderPath, "*.png").OrderBy(f => f).ToArray();
+		if (files.Length == 0)
+		{
+			GD.PrintErr("No files found in the folder");
+			return;
+		}
 
-        if (GetTree().EditedSceneRoot is not Sprite3D sprite3D)
-            return;
+		if (GetTree().EditedSceneRoot is not Sprite3D sprite3D)
+		{
+			GD.PrintErr("Please select a Sprite3D node to import animations");
+			return;
+		}
 
-        var files = Directory.GetFiles(folderPath, "*.png").OrderBy(f => f).ToArray();
-        if (files.Length == 0)
-            return;
+		/* Check this later
+		var animation = new Animation { LoopMode = Animation.LoopModeEnum.Linear };
+		//! Animation tracks, etc
+		//animationPlayer.AddAnimation("GeneratedAnimation", animation);
+		animation.TrackSetPath(animation.AddTrack(Animation.TrackType.Value), "texture");
 
-        var animationPlayer = sprite3D.GetNode<AnimationPlayer>("AnimationPlayer");
-        if (animationPlayer.GetParent() == null)
-            sprite3D.AddChild(animationPlayer);
-
-        var animation = new Animation { LoopMode = Animation.LoopModeEnum.Linear };
-        //! Animation tracks, etc
-        //animationPlayer.AddAnimation("GeneratedAnimation", animation);
-        animation.TrackSetPath(animation.AddTrack(Animation.TrackType.Value), "texture");
-
-        var timeStep = 1.0f / files.Length;
-        for (var i = 0; i < files.Length; i++)
-        {
-            animation.TrackInsertKey(i, i * timeStep, GD.Load<Texture>(files[i]));
-        }
-    }
+		var timeStep = 1.0f / files.Length;
+		for (var i = 0; i < files.Length; i++)
+		{
+			animation.TrackInsertKey(i, i * timeStep, GD.Load<Texture>(files[i]));
+		}
+		*/
+	}
 }
 
 #endif
